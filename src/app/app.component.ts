7612c7091a85710402c318974ad245bac787da26
Franco Montiel <francomontiel@hotmail.com>
2017-02-14 17:27:51 -0400

    Implemented basic modal. Installed ang-material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { Overlay } from 'angular2-modal';
+import { Modal } from 'angular2-modal/plugins/bootstrap';
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <h1>Hello {{name}}</h1>
-    <loading-modal></loading-modal>
-  `,
+	selector: 'my-app',
+	template: `
+		<h1>Hello {{name}}</h1>
+		<button (click)="showBasic()">Basic modal</button>
+		<button (click)="showModded()">Modded modal</button>
+	`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+	name = 'Angular';
+
+	constructor(overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal) {
+		overlay.defaultViewContainer = vcRef;
+	}
+
+	showBasic() {
+		this.modal.alert()
+				.size('lg')
+				.showClose(true)
+				.title('A simple Alert style modal window')
+				.body(`
+						<h4>Alert is a classic (title/body/footer) 1 button modal window that 
+						does not block.</h4>
+						<b>Configuration:</b>
+						<ul>
+							<li>Non blocking (click anywhere outside to dismiss)</li>
+							<li>Size large</li>
+							<li>Dismissed with default keyboard key (ESC)</li>
+							<li>Close wth button click</li>
+							<li>HTML content</li>
+						</ul>
+					`)
+				.open();
+  }
+
+	showModded() {
+		this.modal.alert()
+				.size('sm')
+				.isBlocking(true)
+				.showClose(true)
+				.title('A custom modal')
+				.body(`
+						<h4>Alert is a classic (title/body/footer) 1 button modal window that 
+						does not block.</h4>
+						<b>Configuration:</b>
+					`)
+				.open();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { LoadingModalModule } from './app/loadingmodal.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-platformBrowserDynamic().bootstrapModule(LoadingModalModule);
