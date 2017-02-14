b1ff0134920f365b85002a903414a7deeae18c1c
Franco Montiel <francomontiel@hotmail.com>
2017-02-14 17:27:29 -0400

    Solved systemJS issue for usage.

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -25,7 +25,9 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'angular2-modal':            'npm:angular2-modal',
+      'angular2-modal/plugins/bootstrap': 'npm:angular2-modal/bundles/angular2-modal.bootstrap.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -34,6 +36,10 @@
       },
       rxjs: {
         defaultExtension: 'js'
+      },
+      'angular2-modal': {
+        defaultExtension: 'js',
+        main: '/bundles/angular2-modal.umd.js'
       }
     }
   });
